94d62f6756322de2dd786d2f4655994ed2341416
Russell Goualin <r.goualin@hotmail.com>
2019-12-05 16:11:27 +0000

    signUp handle change

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,15 +1,27 @@
-import React from "react"
+import React, {useState} from "react"
 import Layout from '../core/Layout'
-import {API} from "../config"
 
 
 const Signup = () => {
+    const [values, setValues] = useState ({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    })
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }
+
+
     const signUpForm = () => (
         <form className="box">
             <h1>Signup</h1> 
-            <input type="text" className="" placeholder="Name"/>
-            <input type="email" className="" placeholder="Email"/>
-            <input type="password" className="" placeholder="password"/>    
+            <input onChange={handleChange('name')} type="text" className="" placeholder="Name"/>
+            <input onChange={handleChange('email')} type="email" className="" placeholder="Email"/>
+            <input onChange={handleChange('password')} type="password" className="" placeholder="password"/>    
             <input type="submit" className="" value="Register"/>         
             {/* <button className="btn btn-primary">Signup</button> */}
         </form>
@@ -21,6 +33,7 @@ const Signup = () => {
             // className="container col-md-8 offset-md-2"
             >
             {signUpForm()}
+            {JSON.stringify(values)}
 
     </Layout>
     )
