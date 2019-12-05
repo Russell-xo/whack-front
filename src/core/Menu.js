63133876c2b73b7965bb3b0ea258ca070700c739
Russell Goualin <r.goualin@hotmail.com>
2019-12-05 12:00:26 +0000

    Link menu

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -2,16 +2,25 @@ import React from "react"
 import {Link, withRouter} from 'react-router-dom'
 import '../styles.scss'
 
+const isActive = (history, path) => {
+    if(history.location.pathname === path) {
+        return {color: '#ff9900'}
+    }else {
+        return { color: '#8b8b9f'}
+    }
+    }
 
-const Menu = () => (
+
+
+const Menu = ({history}) => (
     
     <header id="top_header" className="clearfix">
         <div className="wrapper">
             <h1 className="logo">Whack<span>T</span>icket</h1>
             <nav id="main_nav">
-                <Link to="/">Home</Link>
-                <Link to="/signin">Signin</Link>
-                <Link to="/signup">Signup</Link>
+                <Link className="nav-link" style={isActive(history, "/")} to="/">Home</Link>
+                <Link className="nav-link" style={isActive(history, "/signin")} to="/signin">Signin</Link>
+                <Link className="nav-link" style={isActive(history, "/signup")} to="/signup">Signup</Link>
                 
             </nav>
             {/* <nav className="nav-item">
