b9a0becb5d56f612b5f1b992446d1836ed472221
Russell Goualin <r.goualin@hotmail.com>
2019-12-06 13:42:38 +0000

    admin user directed to admin dashboard

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,8 @@ import Signin from './user/Signin'
 import Home from './core/Home'
 import PrivateRoute from './auth/PrivateRoute'
 import Dashboard from './user/UserDashboard'
+import AdminRoute from './auth/AdminRoute'
+import AdminDashboard from './user/AdminDashboard'
 
 
 
@@ -16,6 +18,7 @@ const Routes = () => {
             <Route path="/signup" exact component={Signup}/>
             <Route path="/" exact component={Home}/>
             <PrivateRoute path="/user/dashboard" exact component={Dashboard}/>
+            <AdminRoute path="/admin/dashboard" exact component={AdminDashboard}/>
         </Switch>
     </BrowserRouter>
     )
