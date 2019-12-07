d94e14d66a102a9ef209e4bc398660c00cc46496
Russell Goualin <r.goualin@hotmail.com>
2019-12-07 04:09:13 +0000

    constraint on product text

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,29 +1,28 @@
 import React, {useState} from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import ShowImage from './ShowImage'
+import '../styles.scss'
 // import moment from 'moment'
 // import {addItem, updateItem, removeItem} from './cardHelpers'
 
 const Card = ({product}) => {
     return (
         <div className= "col-4 mb-3">
-            <div className="card">
-                <div className="card-header">{product.name}</div>
-                <div className="card-body">
+            <div className="row">
+                <div className="post">
                 <ShowImage item={product} url="product"/>
-                    <p>{product.description}</p>
+                <div className="title">{product.name}</div>
+                    {/* <p>{product.description}</p> */}
                     <p>£{product.price}</p>
                     <Link to="/">
                     <button className="btn btn-outline-primary mt-2 mb-2">
                         View Product
                     </button>
-                    <button className="btn btn-outline-primary mt-2 mb-2">
-                        Add
-                    </button>
+                    
                     </Link>
                 </div>
             </div>
-        </div>
+         </div>
     )
 }
 export default Card
