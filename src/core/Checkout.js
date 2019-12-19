4983e45124f092fc92ad12fa96bf7d6431a1f0ac
Russell Goualin <r.goualin@hotmail.com>
2019-12-19 13:59:19 +0000

    show user address

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -61,6 +61,8 @@ const Checkout = ({products, setRun = f => f, run = undefined}) => {
         </Link>
     )}
 
+    let deliveryAddress = data.address
+
     const buy = () => {
         setData({loading: true})
         let nonce;
@@ -84,7 +86,7 @@ const Checkout = ({products, setRun = f => f, run = undefined}) => {
                     products: products, 
                     transaction_id: response.transaction.id, 
                     amount: response.transaction.amount,
-                    address: data.address
+                    address: deliveryAddress
                 }
 
                 createOrder(userId, token, createOrderData)
